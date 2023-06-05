cc89431561d009db2bbc44fc801b7ec32c93e07b
M K Mandal <mkuser608@gmail.com>
2023-06-05 08:39:52 +0530

    Create Custom hools to fetch Games

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,8 +1,9 @@
-import axios from "axios";
+import axios, { CanceledError } from "axios";
+
 
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
         key: 'b7103bf54678482a840fc3130173a9ff'
     }
-})
+});
